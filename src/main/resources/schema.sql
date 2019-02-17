ee0688a3e3b7d628aafeac60afb3fbb3ddde13d0
Hasoo <vaxzeen@hotmail.com>
2019-02-17 22:49:11 +0900

    change columns position

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,17 @@
 DROP TABLE IF EXISTS `account`;
 CREATE TABLE `account` (
-  `fee` double(11,2) NOT NULL,
-  `groupname` varchar(255) NOT NULL,
   `id` bigint(20) NOT NULL,
-  `account_non_expired` bit(1) NOT NULL,
-  `account_non_locked` bit(1) NOT NULL,
-  `credentials_non_expired` bit(1) NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `enabled` bit(1) NOT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
+  `groupname` varchar(255) NOT NULL,
   `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `fee` double(11,2) NOT NULL,
+  `enabled` bit(1) NOT NULL,
+  `credentials_non_expired` bit(1) NOT NULL,
+  `account_non_locked` bit(1) NOT NULL,
+  `account_non_expired` bit(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
